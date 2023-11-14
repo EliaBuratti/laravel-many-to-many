b8fba2dd57231c05bc8a19e86df5d34615ef88f7
EliaBuratti <elia.buratti.dev@gmail.com>
2023-11-14 16:18:15 +0100

    fix name table migration

--- database/migrations/2023_11_14_145456_create_project_technology_table.php
+++ database/migrations/2023_11_14_145456_create_project_technology_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->unsignedBigInteger('technology_id');
-            $table->foreign('technology_id')->references('id')->on('tecnologies');
+            $table->foreign('technology_id')->references('id')->on('technologies');
 
             $table->unsignedBigInteger('project_id');
             $table->foreign('project_id')->references('id')->on('projects');
